2ed262a2cfc8442b9086ef76de942016f0a32c6d
keerthana <20b01a1242@svecw.edu.in>
2022-02-09 18:50:16 +0530

    firstproject

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import image from "./lizard.jpg";
+import "./styles.css";
+
+function Welcome() {
+    
+  return (
+    <div className="first">
+      <div className="second">
+        <img src={image} />
+        <div className="third">
+          <h2>LIZARD</h2>
+          <p>
+            Lizard are a widespread group of squamate <br />
+            reptiles, with over 6000 species, ranging across <br />
+            all continents except Antartica
+          </p>
+          <a href="">SHARE</a> 
+          
+          <a href = "" >LEARN</a>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<Welcome />, 
+document.getElementById("root"));
